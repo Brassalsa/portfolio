6b40dfa519af6464ab54e50e1666a7e687a80698
Brassalsa <sauravchauhan2k@gmail.com>
2024-01-12 22:17:48 +0530

    refactor(navbar): refactor link with a tags

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,23 +1,20 @@
-import Link from "next/link";
-import React from "react";
-
 const NavBar = () => {
   return (
     <nav className="flex gap-2">
       <span className="flex-1">
-        <Link className="fancy" href={"/#top"}>
+        <a className="fancy" href={"/#top"}>
           Portfolio
-        </Link>
+        </a>
       </span>
-      <Link className="fancy" href={"/#skills"}>
+      <a className="fancy" href={"/#skills"}>
         Skills
-      </Link>
-      <Link className="fancy" href={"/#projects"}>
+      </a>
+      <a className="fancy" href={"/#projects"}>
         Projects
-      </Link>
-      <Link className="fancy" href={"/#contact"}>
+      </a>
+      <a className="fancy" href={"/#contact"}>
         Socials
-      </Link>
+      </a>
     </nav>
   );
 };
