a003c4090f713a51fb9502cebc1b1f42a87cd5f2
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-19 11:50:29 +0530

    project card styles changes, remove files

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -12,19 +12,23 @@ type Props = {
 
 const ProjectCard = (p: Props) => {
   return (
-    <div className="flex flex-col gap-2 justify-center items-center h-80 w-60 hover:opacity-90 transition-all duration-200 p-4 rounded-md bg-white/10 relative group overflow-y-hidden">
-      <Image
-        src={p.img || "/svg/default.svg"}
-        height={200}
-        width={200}
-        alt=""
-        className="justify-self-start h-auto w-auto"
-      />
+    <div className="flex flex-col h-[320px] w-60 justify-center hover:opacity-90 transition-all duration-200 p-0 rounded-md bg-white/10 relative group overflow-y-hidden cursor-pointer border border-slate-800">
+      <div className="p-2 space-y-2 relative flex-1">
+        <Image
+          src={p.img || "/svg/default.svg"}
+          height={200}
+          width={200}
+          alt=""
+          className="size-auto aspect-video sticky top-0"
+        />
 
-      <div className="break-words flex flex-col gap-2 justify-center items-center">
-        <h4 className="font-semibold text-lg">{p.label}</h4>
-        {p.children}
+        <div className="break-words space-y-2 text-center">
+          <h4 className="font-semibold text-lg">{p.label}</h4>
+          <p>{p.children}</p>
+        </div>
       </div>
+
+      {/*on hover show this element*/}
       <div className="absolute translate-y-full group-hover:translate-y-0 group-focus:translate-y-0 transition-transform duration-700 bg-black/75 h-full w-full flex gap-4 justify-center items-center">
         <Link
           href={p.github}
